ed5f3550631d5455e49c0f10755eab5c1c9306f1
jiradawi <cutiemoon08@yahoo.com>
2019-08-06 14:14:50 +0700

    add controller and helper

--- force-app/main/default/aura/IndicatorBadges/IndicatorBadgesController.js
+++ force-app/main/default/aura/IndicatorBadges/IndicatorBadgesController.js
@@ -1,9 +1,12 @@
 ({
- doInit : function(component, event, helper) {
- var sObj = component.get("v.sObjectName");
- if(sObj){
- helper.getLabelForRecord(component, sObj);
- }
- },
- //future code here
-})
+    doInit : function(component, event, helper) {
+    var recId = component.get("v.recordId");
+    var sObj = component.get("v.sObjectName");
+    if(sObj){
+    helper.getLabelForRecord(component, sObj);
+    helper.getBadgesForRecord(component, recId, sObj);
+    }
+    },
+    //future code here
+   })
+   
